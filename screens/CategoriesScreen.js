ca4934e64a1aa6101dcc0a3f2344c0d6ce0dabc8
kimmingyu <minku4820@gmail.com>
2022-11-18 00:05:52 +0900

    화면 간 데이터 전달 위해 라우트 매개변수로 작업

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import { CATEGORIES } from "../data/dummy-data";
 const CategoriesScreen = ({ navigation }) => {
   const renderCategoryitem = (itemData) => {
     function pressHandler() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
     }
     return (
       <CategoryGridTitle
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 };
