219e89f1a1746a3baaa37ad5e527f7b1c6eab47c
kimmingyu <minku4820@gmail.com>
2022-11-17 23:53:57 +0900

    두 화면 사이 내비게이션 구현

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,11 +2,20 @@ import { FlatList } from "react-native";
 import CategoryGridTitle from "../components/CategoryGridTitle";
 import { CATEGORIES } from "../data/dummy-data";
 
-const renderCategoryitem = (itemData) => {
-  return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color} />;
-};
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryitem = (itemData) => {
+    function pressHandler() {
+      navigation.navigate("MealsOverview");
+    }
+    return (
+      <CategoryGridTitle
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
 
-const CategoriesScreen = () => {
   return (
     <FlatList
       data={CATEGORIES}
