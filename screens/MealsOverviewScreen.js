d54e66c858e03093be2fcdfda434a47f2f8d0219
kimmingyu <minku4820@gmail.com>
2022-11-18 21:50:50 +0900

    외부 경로로 이미지 띄우기, 스타일링

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import MealItem from "../components/MealItem";
+import { MEALS } from "../data/dummy-data";
 
 const MealsOverviewScreen = ({ route }) => {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    const item = itemData.item;
+
+    const mealItmeProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return <MealItem {...mealItmeProps} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
